7c28f9bf0eb2103a0404f61f2af0943f15082ce0
bragelbytes <joebragel@gmail.com>
2024-03-13 14:31:44 -0700

    Update OmniTests.swift

--- Tests/OmniTests/OmniTests.swift
+++ Tests/OmniTests/OmniTests.swift
@@ -1,4 +1,7 @@
 import XCTest
+
+import Logging
+
 @testable import Omni
 
 final class OmniTests: XCTestCase {
@@ -8,5 +11,41 @@ final class OmniTests: XCTestCase {
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+    
+    func testOmniClientToEchoServe() async
+    {
+        do
+        {
+            let omniClientConfigPath = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("OmniClientConfig.json")
+            
+            let clientMessage = "pass"
+            let logger = Logger(label: "Omni")
+            
+            let omniClient = Omni(logger: logger)
+            
+            guard let omniClientConfig = OmniConfig.ClientConfig(path: omniClientConfigPath.path) else
+            {
+                XCTFail()
+                return
+            }
+                    
+            let omniClientConnection = try await omniClient.connect(config: omniClientConfig)
+            
+            print("OmniClient connected to server.")
+            
+            try await omniClientConnection.writeString(string: clientMessage)
+            
+            print("OmniClient wrote to server.")
+            
+            let response = try await omniClientConnection.read()
+            print("Server response: \(response.string)")
+            
+            XCTAssertEqual(clientMessage, response.string)
+        }
+        catch
+        {
+            XCTFail()
+        }
     }
 }
