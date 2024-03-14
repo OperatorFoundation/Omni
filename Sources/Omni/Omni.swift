693acb99b8f6fec137c648f8d5770ad17835bb07
consuelita <adelita@operatorfoundation.org>
2024-03-14 14:32:03 -0500

    Update Omni.swift

--- Sources/Omni/Omni.swift
+++ Sources/Omni/Omni.swift
@@ -12,12 +12,12 @@ import TransmissionAsync
 
 public class Omni
 {
-    let replicant: ReplicantAsync
+    let replicant: Replicant
     let logger: Logger
     
     public init(logger: Logger)
     {
-        self.replicant = ReplicantAsync(logger: logger)
+        self.replicant = Replicant(logger: logger)
         self.logger = logger
     }
     
@@ -25,18 +25,18 @@ public class Omni
     {
         let serverToneburst = Omnitone(.POP3Server)
         let polishConfig = PolishServerConfig(serverAddress: config.serverAddress, serverPrivateKey: config.serverPrivateKey)
-        let replicantConfig = try ReplicantConfigAsync.ServerConfig(serverAddress: config.serverAddress, polish: polishConfig, toneBurst: serverToneburst)
-        return try ReplicantListenerAsync(config: replicantConfig, logger: logger)
+        let replicantConfig = try ReplicantConfig.ServerConfig(serverAddress: config.serverAddress, polish: polishConfig, toneBurst: serverToneburst)
+        return try ReplicantListener(config: replicantConfig, logger: logger)
     }
     
     public func connect(config: OmniConfig.ClientConfig) async throws -> AsyncConnection
     {
         let clientToneburst = Omnitone(.POP3Client)
         let polishConfig = PolishClientConfig(serverAddress: config.serverAddress, serverPublicKey: config.serverPublicKey)
-        let replicantConfig = try ReplicantConfigAsync.ClientConfig(serverAddress: config.serverAddress, polish: polishConfig, toneBurst: clientToneburst)
+        let replicantConfig = try ReplicantConfig.ClientConfig(serverAddress: config.serverAddress, polish: polishConfig, toneBurst: clientToneburst)
         let network = try await AsyncTcpSocketConnection(config.serverIP, Int(config.serverPort), logger)
         
-        return try await replicant.replicantClientTransformationAsync(connection: network, config: replicantConfig, logger: logger)
+        return try await replicant.replicantClientTransformation(connection: network, config: replicantConfig, logger: logger)
     }
 
 }
