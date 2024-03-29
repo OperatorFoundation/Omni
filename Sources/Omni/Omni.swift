8645bd0479e3c5fb76f3d39d4cb276c349689cde
bragelbytes <joebragel@gmail.com>
2024-03-29 16:38:30 -0700

    updated toneburst

--- Sources/Omni/Omni.swift
+++ Sources/Omni/Omni.swift
@@ -21,7 +21,7 @@ public class Omni
     
     public func listen(config: OmniServerConfig) throws -> AsyncListener
     {
-        let serverToneburst = Omnitone(.POP3Server)
+        let serverToneburst = Omnitone(.POP3Client)
         let polishConfig = PolishServerConfig(serverAddress: config.serverAddress, serverPrivateKey: config.serverPrivateKey)
         let replicant = Replicant(logger: self.logger, polish: polishConfig, toneburst: serverToneburst)
         return try ReplicantListener(replicant: replicant, serverIP: config.serverIP, serverPort: Int(config.serverPort), logger: self.logger)
@@ -29,12 +29,11 @@ public class Omni
     
     public func connect(config: OmniClientConfig) async throws -> AsyncConnection
     {
-        let clientToneburst = Omnitone(.POP3Client)
+        let clientToneburst = Omnitone(.POP3Server)
         let polishConfig = PolishClientConfig(serverAddress: config.serverAddress, serverPublicKey: config.serverPublicKey)
         let replicant = Replicant(logger: self.logger, polish: polishConfig, toneburst: clientToneburst)
         let network = try await AsyncTcpSocketConnection(config.serverIP, Int(config.serverPort), logger)
         
         return try await replicant.replicantClientTransformation(connection: network)
     }
-
 }
